043dab58379081ad85c535379a883e2724870fcc
Ram Krishnan <ramkrishnan@live.com>
2022-05-18 16:14:06 +0530

    Update Homepage with all components

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import AddTransaction from '../../components/Home/AddTransaction';
+import CreateWallet from '../../components/Home/CreateWallet';
+import WalletDetails from '../../components/Home/WalletDetails';
+import { API_URL } from '../../constants';
+
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
+  const walletId = localStorage.getItem('walletId');
+
+  const [walletIdExists, setWalletIdExists] = useState(walletId ? true : false);
+  const [walletDetails, setWalletDetails] = useState();
+
+  const getWalletDetails = () => {
+    axios
+      .get(`${API_URL}/wallet/${walletId}`)
+      .then((rs) => setWalletDetails(rs.data.response));
+  };
+
+  useEffect(() => {
+    if (walletId) {
+      getWalletDetails();
+    }
+  }, []);
+
+  return !walletIdExists ? (
+    <CreateWallet setWalletIdExists={setWalletIdExists} />
+  ) : (
+    <>
+      <WalletDetails walletDetails={walletDetails} />
+      <AddTransaction />
+    </>
   );
 };
 
